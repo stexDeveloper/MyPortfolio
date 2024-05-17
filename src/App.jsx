bcc356148fd28e64a0bd618ef56bfe8cd7d6a683
Stex <balkanwebdev96@gmail.com>
2024-05-17 21:07:13 +0200

    hero_about_section_done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import Layout from "./layout/Layout";
 import "./App.scss";
+import AboutIllustration from "./assets/icons/AboutIllustration";
 
 function App() {
   return (
     <Layout>
       <section id="hero">
-        <div className="container">Hero</div>
+        <div className="code-deco">
+          <img src="/code_deco.png" alt="react code picture" />
+        </div>
+        <div className="container">
+          <div className="heading">
+            <h1>Web Site Portfolio for Web Development</h1>
+          </div>
+          <div className="action-buttons">
+            <button className="first">My Portfolio</button>
+            <button className="second">Download CV</button>
+          </div>
+        </div>
       </section>
-      <section id="about">About</section>
+      <section id="about">
+        <div className="container">
+          <div className="wrapper">
+            <div className="illustration">
+              <AboutIllustration />
+            </div>
+            <div className="about-text">
+              <div className="badge">About</div>
+              <div className="text">
+                <h2>Web Development</h2>
+                <p>
+                  I am Kilian, a passionate freelancer from Hamburg, Germany and
+                  the founder of LOKKEE STUDIOS, bringing you web development
+                  and design from the future. My expertise is developing
+                  next-level websites and web applications including full
+                  frontend design.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <section id="work">Work</section>
       <section id="skills">Skills</section>
       <section id="testimonials">Testimonials</section>
